752ed0ad4a77238513169523a5e816f5f6c27c33
tianbo <tianbo@hey.com>
2022-11-22 22:57:41 -0800

    Typespecs

---
+++ string-helpers.ex
@@ -0,0 +1,8 @@
+defmodule StringHelpers do
+  @type word() :: String.t()
+
+  @spec long_word?(word()) :: boolean()
+  def long_word?(word) when is_binary(word) do
+    String.length(word) > 8
+  end
+end
